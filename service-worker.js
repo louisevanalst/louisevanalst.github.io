c5962bc6ec3721942c22f98b3613d189891d23ad
Louise van Alst <62909479+louisevanalst@users.noreply.github.com>
2023-10-12 14:06:47 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,4 +2,10 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 // Gebruik `workbox.routing.registerRoute()`...
+workbox.routing.registerRoute(
+  capture: new RegExp('/*'),
+  handler: new workbox.strateies.NetworkFirst({
+  cacheName: 'EEN_CACHE_KEY'
+})
+)
 
